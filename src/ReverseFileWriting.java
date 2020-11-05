c3a1aba9497e3638d99b560b4882d840ae419c6e
Yury <ryv8@tut.by>
2020-11-05 16:19:07 +0200

    ReverseFileWriting

---
+++ src/ReverseFileWriting.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseFileWriting {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = reader.readLine();
+        String file2 = reader.readLine();
+
+        FileInputStream fileInputStream = new FileInputStream(file1);
+        FileOutputStream fileOutputStream = new FileOutputStream(file2);
+
+        List<Integer> list = new ArrayList<>();
+
+        while (fileInputStream.available() > 0){
+            list.add(fileInputStream.read());
+        }
+
+        Collections.reverse(list);
+
+        for (Integer integer : list) {
+            fileOutputStream.write(integer);
+        }
+
+        fileOutputStream.close();
+        fileInputStream.close();
+    }
+}
