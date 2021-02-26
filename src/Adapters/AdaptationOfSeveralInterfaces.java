4f6068b05f8475c1d664b265159e431ad4c59cb7
Yury <ryv8@tut.by>
2021-02-26 16:15:51 +0200

    AdaptationOfSeveralInterfaces

---
+++ src/Adapters/AdaptationOfSeveralInterfaces.java
@@ -0,0 +1,54 @@
+package Adapters;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AdaptationOfSeveralInterfaces {
+
+    public static Map<String, String> countries = new HashMap<String, String>();
+
+    static {
+        countries.put("UA", "Ukraine");
+        countries.put("RU", "Russia");
+        countries.put("CA", "Canada");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public static class IncomeDataAdapter implements Customer, Contact {
+
+        private IncomeData data;
+        IncomeDataAdapter(IncomeData data){
+            this.data = data;
+        }
+    }
+
+
+    public static interface IncomeData {
+        String getCountryCode();        //For example: UA
+
+        String getCompany();            //For example: JavaRush Ltd.
+
+        String getContactFirstName();   //For example: Ivan
+
+        String getContactLastName();    //For example: Ivanov
+
+        int getCountryPhoneCode();      //For example: 38
+
+        int getPhoneNumber();           //For example: 501234567
+    }
+
+    public static interface Customer {
+        String getCompanyName();        //For example: JavaRush Ltd.
+
+        String getCountryName();        //For example: Ukraine
+    }
+
+    public static interface Contact {
+        String getName();               //For example: Ivanov, Ivan
+
+        String getPhoneNumber();        //For example: +38(050)123-45-67
+    }
+}
